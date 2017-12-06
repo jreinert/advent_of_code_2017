f4b3788b957057dcfdd37d120c4e9d30b1c8a633
Joakim Reinert <mail@jreinert.com>
2017-12-06 13:10:11 +0100

    add generation action to cli to create a days source from template

--- src/cli.cr
+++ src/cli.cr
@@ -1,7 +1,16 @@
 require "./days"
 require "./days/*"
+require "ecr"
 
 module Cli
+  USAGE =<<-USAGE
+    Usage: #{PROGRAM_NAME} <action> <day> [<part>]\n
+      Actions:\n
+      run : run <day> <part> and print result\n
+      gen : generate <day> source
+    USAGE
+
+
   def self.run(day, part)
     {% begin %}
       case day
@@ -18,10 +27,24 @@ module Cli
       end
     {% end %}
   end
+
+  def self.generate(day)
+    file_path = File.expand_path("../days/day_#{day.rjust(2, '0')}.cr", __FILE__)
+
+    File.open(file_path, "w+") do |f|
+      ECR.embed("day_template.ecr", f)
+    end
+  end
 end
 
-abort("Usage: #{PROGRAM_NAME} <day> <part>") unless ARGV.size == 2
+abort(Cli::USAGE) if ARGV.size < 2
+action, day = { ARGV.shift, ARGV.shift }
 
-day, part = ARGV
-
-puts Cli.run(day, part)
+case action
+when "run"
+  part = ARGV.shift?
+  abort(Cli::USAGE) unless part
+  puts Cli.run(day, part)
+when "gen" then Cli.generate(day)
+else abort(Cli::USAGE)
+end
